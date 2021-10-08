466e03cb7659df2d278997e189c36727562bb49b
Leonel Chaves <lchaves@certant.com>
2021-10-08 20:25:11 -0300

    Se suma método a LoanRepository
    Se agrega método para buscar todos los Loan, de acuerdo a un userId

--- src/main/java/com/example/loanapi/repository/LoanRepository.java
+++ src/main/java/com/example/loanapi/repository/LoanRepository.java
@@ -1,9 +1,12 @@
 package com.example.loanapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.loanapi.model.Loan;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
 
+	Page<Loan> findAllByUser(Long userId, Pageable pageable);
 }
