7cf572a1b97e3e38a67e0067fe2ab83e9f58f54b
Leonel Chaves <lchaves@certant.com>
2021-10-08 19:03:39 -0300

    Se define model de Loan
    Se declaran los atributos id, total y user

---
+++ src/main/java/com/example/loanapi/model/Loan.java
@@ -0,0 +1,129 @@
+package com.example.loanapi.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="LOAN")
+public class Loan {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	private Long id;
+	
+	@Column(name="TOTAL")
+	private Double total;
+	
+	private User user;
+
+	public Loan() {
+		
+		super();
+	}
+
+	public Loan(Long id, Double total) {
+		
+		super();
+		this.id = id;
+		this.total = total;
+	}
+
+	public Long getId() {
+		
+		return id;
+	}
+
+	public void setId(Long id) {
+		
+		this.id = id;
+	}
+
+	public Double getTotal() {
+		
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		
+		this.total = total;
+	}
+
+	public User getUser() {
+		
+		return user;
+	}
+
+	public void setUser(User user) {
+		
+		this.user = user;
+	}
+
+	@Override
+	public int hashCode() {
+		
+		return Objects.hash(id, total, user);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (this == obj)
+			return true;
+		
+		if (obj == null)
+			return false;
+		
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Loan other = (Loan) obj;
+		
+		return Objects.equals(id, other.id) && Objects.equals(total, other.total) && Objects.equals(user, other.user);
+	}
+	
+	
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("{\"class\": ").append("\"Loan\"");
+
+		if (id != null) {
+			
+			sb.append(", ");
+			sb.append("\"id\": ");
+			sb.append("\"");
+			sb.append(id);
+			sb.append("\"");
+		}
+		
+		if (total != null) {
+			
+			sb.append(", ");
+			sb.append("\"total\": ");
+			sb.append("\"");
+			sb.append(total);
+			sb.append("\"");
+		}
+		
+		if (user != null) {
+			
+			sb.append(", ");
+			sb.append("\"user\": ");
+			sb.append("\"");
+			sb.append(user);
+			sb.append("\"");
+		}
+		
+		sb.append("}");	
+		
+		return sb.toString();
+	}	
+}
