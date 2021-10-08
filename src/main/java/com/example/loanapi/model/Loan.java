038064582a74916c287b3c347bc4adff0c2763e1
Leonel Chaves <lchaves@certant.com>
2021-10-08 20:01:58 -0300

    Se agrega relación faltante en Loan
    Como la relación es OneToMany de User para con Loan,
    faltaba agregar la relación inversa en Loan

--- src/main/java/com/example/loanapi/model/Loan.java
+++ src/main/java/com/example/loanapi/model/Loan.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +21,7 @@ public class Loan {
 	@Column(name="TOTAL")
 	private Double total;
 	
+	@ManyToOne
 	private User user;
 
 	public Loan() {
