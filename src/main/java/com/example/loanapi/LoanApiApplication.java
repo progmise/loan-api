93f0f0e484df7851b9621833c3b62c4d4042b17f
Leonel Chaves <lchaves@certant.com>
2021-10-09 12:10:37 -0300

    Se agrega package a escanear por los beans

--- src/main/java/com/example/loanapi/LoanApiApplication.java
+++ src/main/java/com/example/loanapi/LoanApiApplication.java
@@ -2,8 +2,10 @@ package com.example.loanapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.loanapi.dao")
 public class LoanApiApplication {
 
 	public static void main(String[] args) {
