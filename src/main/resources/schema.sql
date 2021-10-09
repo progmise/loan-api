24f0cda663df2e6f122d917843d026510d5212aa
Leonel Chaves <lchaves@certant.com>
2021-10-08 21:45:16 -0300

    Se agrega schema.sql
    Se declaran las queries para crear las tablas de LOAN y USER

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS public."USER" (
+	"ID" integer NOT NULL,
+	"EMAIL" character varying(80) NOT NULL,
+	"FIRST_NAME" character varying(25) NOT NULL, 
+	"LAST_NAME" character varying(45) NOT NULL, 
+	CONSTRAINT "USER_PK" PRIMARY KEY ("ID")
+);
+
+CREATE TABLE IF NOT EXISTS public."LOAN" (
+	"ID" integer NOT NULL, 
+	"TOTAL" numeric NOT NULL, 
+	"USER_ID" integer NOT NULL, 
+	CONSTRAINT "LOAN_PK" PRIMARY KEY ("ID"), 
+	CONSTRAINT "LOAN_FK" FOREIGN KEY ("USER_ID")
+		REFERENCES public."USER" ("ID")	
+);
