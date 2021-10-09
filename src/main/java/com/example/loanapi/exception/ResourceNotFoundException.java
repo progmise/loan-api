f7e37722c2d64c516ddcecf281e2c77097c56c5d
Leonel Chaves <lchaves@certant.com>
2021-10-09 20:31:36 -0300

    Se agrega serial

--- src/main/java/com/example/loanapi/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/loanapi/exception/ResourceNotFoundException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value=HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 5002389565249206069L;
 	
 	private String resourceName;
 	private String fieldName;
