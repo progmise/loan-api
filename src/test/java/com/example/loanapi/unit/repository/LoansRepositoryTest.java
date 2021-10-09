530268283542873ce962c1759e771902d68ad291
Leonel Chaves <lchaves@certant.com>
2021-10-09 11:29:00 -0300

    Se elimina import innecesario

--- src/test/java/com/example/loanapi/unit/repository/LoansRepositoryTest.java
+++ src/test/java/com/example/loanapi/unit/repository/LoansRepositoryTest.java
@@ -2,7 +2,6 @@ package com.example.loanapi.unit.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
