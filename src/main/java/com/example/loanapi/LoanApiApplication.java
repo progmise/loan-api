d9672b18f9c1205037283cce7c0795aec7650e64
Leonel Chaves <lchaves@certant.com>
2021-10-09 17:00:36 -0300

    Se agrega bean de ModelMapper
    Se define bean de ModelMapper
    
    Se realiza fix y se suman los packages a analizar para spring

--- src/main/java/com/example/loanapi/LoanApiApplication.java
+++ src/main/java/com/example/loanapi/LoanApiApplication.java
@@ -1,14 +1,25 @@
 package com.example.loanapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-@ComponentScan("com.example.loanapi.dao")
+@ComponentScan({"com.example.loanapi.controller", "com.example.loanapi.service"})
 public class LoanApiApplication {
 
+	@Bean
+	public ModelMapper modelMapper() {
+		
+		return new ModelMapper();
+	}	
+	
 	public static void main(String[] args) {
+		
 		SpringApplication.run(LoanApiApplication.class, args);
 	}
 
