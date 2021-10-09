55563106e8faa4d8379e1c579beacbbfacb20623
Leonel Chaves <lchaves@certant.com>
2021-10-09 02:19:29 -0300

    Se agrega decorator JoinColumn
    Se suma dicho decorator, ya que era necesario indicar el campo/columna
    con el cual se debe hacer el join

--- src/main/java/com/example/loanapi/model/Loan.java
+++ src/main/java/com/example/loanapi/model/Loan.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -16,7 +17,7 @@ import org.springframework.lang.NonNull;
 @Table(name="LOAN")
 public class Loan {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="ID")
 	private Long id;
 	
@@ -26,6 +27,7 @@ public class Loan {
 	
 	@ManyToOne
 	@NonNull
+	@JoinColumn(name="USER_ID")
 	private User user;
 
 	public Loan() {
