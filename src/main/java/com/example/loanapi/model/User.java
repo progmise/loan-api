88aa2950535e6300aa50b3aed13f2cf96b8a08b5
Leonel Chaves <lchaves@certant.com>
2021-10-09 00:52:24 -0300

    Se corrige nombre de identificador USERS a USER

--- src/main/java/com/example/loanapi/model/User.java
+++ src/main/java/com/example/loanapi/model/User.java
@@ -17,7 +17,7 @@ import javax.persistence.Table;
 import org.springframework.lang.NonNull;
 
 @Entity
-@Table(name="USERS")
+@Table(name="USER")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
