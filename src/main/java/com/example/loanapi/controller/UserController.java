55613d1428c196c9721e4f401026ff1373d55fea
Leonel Chaves <lchaves@certant.com>
2021-10-09 21:15:05 -0300

    Se definen endpoints para crear y eliminar un User

--- src/main/java/com/example/loanapi/controller/UserController.java
+++ src/main/java/com/example/loanapi/controller/UserController.java
@@ -1,14 +1,17 @@
 package com.example.loanapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.loanapi.constant.Paths;
 import com.example.loanapi.dto.UserDTO;
+import com.example.loanapi.payload.ApiResponse;
 import com.example.loanapi.service.UserService;
 
 @RestController
@@ -25,4 +28,21 @@ public class UserController {
     	
     	return ResponseEntity.ok().body(userDTO);
     }
+    
+    @RequestMapping(method=RequestMethod.POST)
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+    	
+		UserDTO userResponse = userService.createUser(userDTO);
+    	
+    	return new ResponseEntity<UserDTO>(userResponse, HttpStatus.CREATED);
+    }
+    
+    @RequestMapping(value="{id}", method=RequestMethod.DELETE)
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable(value="id") Long id) {
+    	
+		userService.deleteUserById(id);
+		ApiResponse apiResponse = new ApiResponse(Boolean.TRUE, "User deleted successfully", HttpStatus.OK);
+		
+    	return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.OK);
+    }    
 }
