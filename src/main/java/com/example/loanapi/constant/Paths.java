cfb8ef5bf446ed39a86615308d1fe1cc9e3ddc84
Leonel Chaves <lchaves@certant.com>
2021-10-10 11:10:52 -0300

    Se elimina barra final al path de loans

--- src/main/java/com/example/loanapi/constant/Paths.java
+++ src/main/java/com/example/loanapi/constant/Paths.java
@@ -4,5 +4,5 @@ public class Paths {
 	
     public static final String VERSION = "/api/1.0/";
     public static final String USERS = "users/";
-    public static final String LOANS = "loans/";
+    public static final String LOANS = "loans";
 }
